927ece3fa109d59c4a4b3fc81e8708d1468784b6
Bruno de Souza Ferreira Carneiro <bruno.carneiro@spread.com.br>
2018-07-03 14:22:53 -0300

    implementando método de salvar usuário

--- src/main/java/lab/rest1/api/v1/resource/UserResource.java
+++ src/main/java/lab/rest1/api/v1/resource/UserResource.java
@@ -6,10 +6,7 @@ import lab.rest1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,5 +35,11 @@ public class UserResource {
         List users = new ArrayList<>();
         userService.list().forEach(users::add);
         return new ResponseEntity<List<UserDTO>>(users, HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<UserDTO> save(@RequestBody UserDTO userDTO) {
+        userService.save(userDTO.convert());
+        return new ResponseEntity<UserDTO>(userDTO, HttpStatus.OK);
     }
 }
