9fa691586cb2835373e2fc253abd8b9f688a5df0
Bruno de Souza Ferreira Carneiro <bruno.carneiro@spread.com.br>
2018-07-03 16:24:50 -0300

    implementando recursos da aplicação

--- src/main/java/lab/rest1/service/CrudService.java
+++ src/main/java/lab/rest1/service/CrudService.java
@@ -2,16 +2,28 @@ package lab.rest1.service;
 
 import java.util.List;
 
+/**
+ * ===================================================
+ * Interface que definem métodos CRUD para os serviços
+ * ===================================================
+ * @author bruno.carneiro
+ * @param <T>
+ */
 public interface CrudService<T> {
 
+    /** Persiste */
     void save(T t);
 
+    /** Remove */
     void delete(T t);
 
+    /** Lista */
     List<T> list();
 
+    /** Busca */
     T find(T t);
 
+    /** Busca pelo ID */
     T findById(Long id);
 
 }
--- src/main/java/lab/rest1/service/LocationService.java
+++ src/main/java/lab/rest1/service/LocationService.java
@@ -1,8 +1,13 @@
 package lab.rest1.service;
 
-import lab.rest1.api.v1.model.LocationDTO;
 import lab.rest1.domain.Location;
 
+/**
+ * =================================
+ * Métodos do serviço de localização
+ * =================================
+ * @author bruno.carneiro
+ */
 public interface LocationService {
 
     /** Compartilha a localização */
